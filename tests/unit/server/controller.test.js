cd9814b51d89bd8b230e80ec4922007bf68a005f
periscuelo <periscuelo@gmail.com>
2022-03-19 04:13:35 -0300

    Added unit tests

--- tests/unit/server/controller.test.js
+++ tests/unit/server/controller.test.js
@@ -15,7 +15,7 @@ describe('#Controller - test controller behavior', () => {
         mockFileStream = mock.generateReadableStream(['data'])
     })
 
-    test(`getFileStream - should response with object containing file stream`, async () => {
+    test(`#getFileStream`, async () => {
         const file = 'test.html'
         const expectedType = '.html'
 
@@ -32,5 +32,78 @@ describe('#Controller - test controller behavior', () => {
         expect(stream).toStrictEqual(mockFileStream)
 
         expect(type).toStrictEqual(expectedType)
+    })
+
+    test('#createClientStream', async () => {
+        const mockId = '1'
+
+        jest.spyOn(
+            services,
+            'createClientStream'
+        ).mockReturnValue({
+            id: mockId,
+            clientStream: mockFileStream
+        })
+
+        jest.spyOn(
+            services,
+            'removeClientStream'
+        ).mockReturnValue()
+
+        const { stream, onClose } = controller.createClientStream()
+
+        onClose()
+
+        expect(stream).toStrictEqual(mockFileStream)
+        expect(services.removeClientStream).toHaveBeenCalledWith(mockId)
+        expect(services.createClientStream).toHaveBeenCalled()
+
+    })
+
+    describe('handleCommand', () => {
+        test('command stop', async () => {
+            jest.spyOn(
+                    services,
+                    'stopStreamming'
+            ).mockResolvedValue()
+
+            const data = { command: '   stop   ' }
+            const result = await controller.handleCommand(data)
+
+            expect(result).toStrictEqual({
+                result: 'ok'
+            })
+            expect(services.stopStreamming).toHaveBeenCalled()
+        })
+
+        test('command start', async () => {
+            jest.spyOn(
+                    services,
+                    'startStreamming'
+            ).mockResolvedValue()
+
+            const data = { command: ' START ' }
+            const result = await controller.handleCommand(data)
+
+            expect(result).toStrictEqual({
+                result: 'ok'
+            })
+            expect(services.startStreamming).toHaveBeenCalled()
+        })
+
+        test('non existing command', async () => {
+            jest.spyOn(
+                services,
+                'startStreamming'
+            ).mockResolvedValue()
+
+            const data = { command: ' NON EXISTING ' }
+            const result = await controller.handleCommand(data)
+
+            expect(result).toStrictEqual({
+                result: ''
+            })
+            expect(services.startStreamming).not.toHaveBeenCalled()
+        })
     })
 })
